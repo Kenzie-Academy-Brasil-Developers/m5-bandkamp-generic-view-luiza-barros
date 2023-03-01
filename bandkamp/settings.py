e2ea4071d9be0f71f7a876356c2aed1406ee03df
Luiza Barros <thecoderana@gmail.com>
2023-03-01 15:52:48 -0300

    feat: add task 1 complete

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -12,6 +12,22 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+import dotenv
+
+dotenv.load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
+    }
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,14 +100,6 @@ WSGI_APPLICATION = "bandkamp.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- users/serializers.py
+++ users/serializers.py
@@ -3,8 +3,21 @@ from rest_framework.validators import UniqueValidator
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "is_superuser",
+            "last_name",
+            "first_name",
+            "password",
+            "username",
+            "email",
+        ]
+        read_only_fields = ["is_superuser"]
+        extra_kwargs = {"password": {"write_only": True}}
+
     username = serializers.CharField(
         validators=[
             UniqueValidator(
@@ -16,10 +29,6 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())],
     )
-    password = serializers.CharField(write_only=True)
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    is_superuser = serializers.BooleanField(read_only=True)
 
     def create(self, validated_data: dict) -> User:
         return User.objects.create_superuser(**validated_data)
