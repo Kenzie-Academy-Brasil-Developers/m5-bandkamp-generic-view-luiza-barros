3d3655c4b41eee33175f9ee2d73d31ba48d6dc93
Luiza Barros <thecoderana@gmail.com>
2023-03-03 14:43:02 -0300

    feat: add songview with generic view

--- albums/views.py
+++ albums/views.py
@@ -1,13 +1,11 @@
-from rest_framework.views import APIView, status, Response
 from .models import Album
 from .serializers import AlbumSerializer
-from rest_framework.pagination import PageNumberPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.generics import ListCreateAPIView
 
 
-class AlbumView(ListCreateAPIView, PageNumberPagination):
+class AlbumView(ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly]
 
